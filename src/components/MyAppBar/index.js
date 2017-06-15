29cfad6b5b3cde5d9b3e32d89dd1524831f4a449
Valentin Brosseau <c4software@gmail.com>
2017-06-15 22:45:05 +0200

    Init application

---
+++ src/components/MyAppBar/index.js
@@ -0,0 +1,42 @@
+import "./index.css";
+
+import React, {Component} from 'react';
+
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import IconButton from 'material-ui/IconButton';
+import Menu from "material-ui/svg-icons/navigation/menu";
+import MenuItem from 'material-ui/MenuItem';
+import {indigo500} from 'material-ui/styles/colors';
+
+class MyAppBar extends Component{
+  state = {open: false};
+  
+  handleToggle = () => {
+      this.setState({open: !this.state.open});
+  };
+
+  go = (action) => {
+      location.hash = action;
+      this.setState({open: false});
+  };
+
+  render(){
+    return (
+      <div>
+        <Drawer docked={false} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
+            <div style={{height: "67px", backgroundColor: indigo500}} />
+            <MenuItem onTouchTap={() => this.go("/settings")}>Paramètres</MenuItem>
+        </Drawer>
+        <AppBar
+            className="appBar"
+            title="Thermal Hue App"
+            onLeftIconButtonTouchTap={this.handleToggle}
+            iconElementLeft={<IconButton><Menu /></IconButton>}
+        />
+      </div>
+    )
+  }
+}
+
+export default MyAppBar
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {indigo500} from "material-ui/styles/colors"
+
+const theme = getMuiTheme({
+    palette: {
+        primary1Color: indigo500,
+    }
+});
+
+export default theme;
