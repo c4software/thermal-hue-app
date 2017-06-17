6ce6b76b4cc103dc2c4a492466416531b70aced2
Valentin Brosseau <c4software@gmail.com>
2017-06-17 13:33:57 +0200

    Add minimal temperature recuperation views

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,10 +1,42 @@
+import "./index.css";
+
 import React, { Component } from 'react';
 
-class Home extends Component {
+class Home extends Component {  
+
+  state = {
+    data: {}
+  }
+
+  constructor(props) {
+    super(props);
+    this.url_data = localStorage.getItem("url_data");
+  }
+
+  get = () => {
+    fetch(this.url_data + "?get", {mode: "cors"})
+    .then(response => response.json())
+    .then(j => this.setState({data: j.data}))
+    .catch(d => console.error(d));
+  }
+
+  componentDidMount = () => {
+    this.get();
+  }
+
+  display_value = () => {
+    if (this.state.data.last !== undefined){
+      return this.state.data.last.value
+    }else{
+      return "--"
+    }
+  }
+
   render() {
     return (
       <div>
-        <p>Todo</p>
+        <h1 className="temp">{this.display_value()}</h1>
+        <h3>Todo trending</h3>
       </div>
     );
   }
