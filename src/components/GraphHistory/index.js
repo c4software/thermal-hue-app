25aa491c41fc43dd6ce965b3f12f6c331d366227
Valentin Brosseau <c4software@gmail.com>
2017-06-19 23:23:25 +0200

    Use de « indigo500 » color to draw the graph line

--- src/components/GraphHistory/index.js
+++ src/components/GraphHistory/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid} from "recharts";
+import {indigo500} from "material-ui/styles/colors";
 
 class GraphHistory extends Component{
   render = () => {
@@ -10,7 +11,7 @@ class GraphHistory extends Component{
          <CartesianGrid strokeDasharray="3 3" />
          <Tooltip />
          <YAxis width={40} tickFormatter={(v) => v.toFixed(1)} allowDecimals={false} domain={['dataMin - 1', 'dataMax + 1']} />
-         <Line dot={false} type="monotone" dataKey="value" stroke="#82ca9d" />
+         <Line dot={false} type="monotone" dataKey="value" stroke={indigo500} />
         </LineChart>
       </ResponsiveContainer>
     );
