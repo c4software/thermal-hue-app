28ce4dc58477bd2124c579e8efad17106bb50936
Valentin Brosseau <c4software@gmail.com>
2017-06-19 23:22:19 +0200

    Add touch trigger to refresh the temperature data

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -8,6 +8,8 @@ import {connect} from "react-redux";
 import {indigo500} from "material-ui/styles/colors";
 import GraphHistory from "../../components/GraphHistory";
 
+import {get_remote_data} from "../../libs";
+
 class Home extends Component {
 
   display_value = () => {
@@ -42,7 +44,7 @@ class Home extends Component {
     return (
       <div className="dataContainer">
         <div className="tempContainer" style={{backgroundColor: indigo500}}>
-          <div className="temp">
+          <div className="temp" onTouchTap={get_remote_data}>
             {this.display_value()}
             {this.display_trend()}
           </div>
