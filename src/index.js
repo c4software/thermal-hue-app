a9c258ad8ed9a09a1bb9ec4ecc7bdb0453757c31
Valentin Brosseau <c4software@gmail.com>
2017-06-23 23:05:26 +0200

    Wait for the device ready event to start the application

--- src/index.js
+++ src/index.js
@@ -14,11 +14,25 @@ import i18n from './i18n';
 injectTapEventPlugin();
 get_remote_data();
 
-ReactDOM.render(
-  <I18nextProvider i18n={i18n}>
-    <Provider store={store}>
-      {routes}
-    </Provider>
-  </I18nextProvider>,
-  document.getElementById('root')
-);
+const isCordovaApp = (typeof window.cordova !== "undefined");
+const init = () => {
+  ReactDOM.render(
+    <I18nextProvider i18n={i18n}>
+      <Provider store={store}>
+        {routes}
+      </Provider>
+    </I18nextProvider>,
+    document.getElementById('root')
+  );
+}
+
+// Wait for the deviceready event to start the render
+document.addEventListener("deviceready", () => {
+  console.log("Device is ready, Render the App");
+  init();
+});
+
+// If we are not in Cordova, manually trigger the deviceready event
+if (!isCordovaApp){
+  document.dispatchEvent(new CustomEvent("deviceready", {}));
+}
