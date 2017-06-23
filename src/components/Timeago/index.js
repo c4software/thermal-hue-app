ade250a4c05b275401fc913b12f0876987a83253
Valentin Brosseau <c4software@gmail.com>
2017-06-23 23:39:42 +0200

    Use local language for moment too

--- src/components/Timeago/index.js
+++ src/components/Timeago/index.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import moment from "moment";
 import {connect} from "react-redux";
+import i18n from "../../i18n";
 
 class Timeago extends Component{
   state = {timeago: "No data"};
 
   componentDidMount = () => {
+    moment.lang(i18n.language);
     this.timerId = setInterval(() => {
       this.setState({reload: Math.random()});
     }, 30000);
