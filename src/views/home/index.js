7a10e5635dc9060ece3c2b7e97c17cfa921b6a7c
Valentin Brosseau <c4software@gmail.com>
2017-06-25 21:50:23 +0200

    Add pull to refresh and sync the RfreshIndicator with the loading indicator

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -7,8 +7,18 @@ import ArrowDropUp from "material-ui/svg-icons/navigation/arrow-drop-up";
 import {connect} from "react-redux";
 import {indigo500} from "material-ui/styles/colors";
 import GraphHistory from "../../components/GraphHistory";
+import {get_remote_data} from "../../libs";
+
+import ReactPullToRefresh from "react-pull-to-refresh";
+import RefreshIndicator from 'material-ui/RefreshIndicator';
 
 class Home extends Component {
+
+  handleRefresh(resolve, reject) {
+    get_remote_data();
+    resolve();
+  }
+
 
   display_value = () => {
     if (this.props.data.last !== undefined){
@@ -38,15 +48,24 @@ class Home extends Component {
      return trend;
   }
 
+  displayLoading = () => {
+    return (
+      <div style={{position: 'absolute', top: 80, left: 0, right: 0}}>
+        <RefreshIndicator size={50} left={-20} top={0} status={this.props.ajax_in_progress?"loading":"hide"} style={{marginLeft: '50%'}} />
+      </div>
+    )
+  }
+
   render = () => {
     return (
       <div className="dataContainer">
-        <div className="tempContainer" style={{backgroundColor: indigo500}}>
+        {this.displayLoading()}
+        <ReactPullToRefresh loading={null} icon={null} onRefresh={this.handleRefresh} className="tempContainer" style={{backgroundColor: indigo500}}>
           <div className="temp">
             {this.display_value()}
             {this.display_trend()}
           </div>
-        </div>
+        </ReactPullToRefresh>
         <div className="graphContainer">
           <GraphHistory data={this.props.data.history} />
         </div>
@@ -57,6 +76,7 @@ class Home extends Component {
 
 export default connect(function(state){
     return {
-        data: state.data
+        data: state.data,
+        ajax_in_progress: state.ajax_in_progress
     }
 })(Home)
