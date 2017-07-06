8461fc52c1ac8c5908ae5ad3ba0371c863d63a02
Valentin Brosseau <c4software@gmail.com>
2017-07-06 21:52:42 +0200

    Change Language detector according documentation

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,9 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import LngDetector from 'i18next-browser-languagedetector';
 import {locales} from "./locales/";
 
 i18n
-  .use(LanguageDetector)
+  .use(LngDetector)
   .init({
     fallbackLng: 'en',
     debug: false,
