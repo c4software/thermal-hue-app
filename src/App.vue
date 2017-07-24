b603c646c6496e3ebc35e17af24febe1fd502452
Valentin Brosseau <c4software@gmail.com>
2017-07-24 22:40:56 +0200

    Init projet version VueJS

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-navigation-drawer></v-navigation-drawer>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">Thermal Hue App</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu bottom left style="margin-right:0">
+          <v-btn icon slot="activator" dark>
+            <v-icon>more_vert</v-icon>
+          </v-btn>
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-title>Rafraîchir</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+      </v-menu>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </main>
+  </v-app>
+
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
