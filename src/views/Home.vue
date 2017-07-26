597ff988d34ae32dfd6c03eda1d26932786d71d3
Valentin Brosseau <c4software@gmail.com>
2017-07-26 22:23:38 +0200

    Add base template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
+  <div class="dataContainer">
+      <div class="tempContainer indigo">
+        <div class="temp">22</div>
+      </div>
+      <div class="graphContainer">
 
+      </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +14,28 @@
     name: 'home'
   }
 </script>
+
+<style>
+  .dataContainer{
+    height: calc(100vh - 64px);
+  }
+
+  .graphContainer{
+    height: 60%;
+  }
+
+  .temp{
+    text-align: center;
+    font-size: 5rem;
+    margin-bottom: 0;
+  }
+
+  .tempContainer{
+      height: 40%;
+      color: white;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+  }
+</style>
