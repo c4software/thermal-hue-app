f8a4a32f2fa62a4a4e559fcc1102e77d7abb733e
Valentin Brosseau <vbrosseau@afone.com>
2017-07-26 17:45:30 +0200

    Add stub component

---
+++ src/components/GraphHistory.vue
@@ -0,0 +1,10 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'graphHistory',
+    props: ['last']
+  }
+</script>
---
+++ src/components/Timeago.vue
@@ -0,0 +1,10 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'timeago',
+    props: ['last']
+  }
+</script>
