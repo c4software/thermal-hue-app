36cadbdcc863c5fc84761e166dff8404421a67cb
Valentin Brosseau <c4software@gmail.com>
2017-07-27 23:19:13 +0200

    Add default mutation

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex)
 const state = {
   loading: false,
   data: JSON.parse((localStorage.getItem("data")||"{}")),
-  url_data: "",
+  url_data: (localStorage.getItem("url_data")||""),
   roomChooserOpen: false,
   selectedRoom: (localStorage.getItem("selectedRoom")||""),
   roomList: JSON.parse((localStorage.getItem("room")||"[]")),
@@ -20,7 +20,45 @@ const getters = {
 const actions = {}
 const mutations = {
   setLoading (state, boolState) {
-    state.loading = boolState
+    state.loading = boolState;
+  },
+  setData (state, data) {
+    localStorage.setItem("data", JSON.stringify(data));
+    state.data = data;
+  },
+  setUrl (state, url) {
+    localStorage.setItem("url_data", url);
+    state.url_data = url;
+  },
+  toggleRoomChooser (state) {
+    state.roomChooserOpen = !state.roomChooserOpen;
+  },
+  selectRoom (state, roomName) {
+    localStorage.setItem("selectedRoom", roomName);
+    state.selectedRoom = roomName;
+  },
+  setRoomList (state, roomList) {
+    localStorage.setItem("room", JSON.stringify(roomList));
+    state.roomList = roomList;
+  },
+  disableRoom (state, roomName) {
+    // Disable the current selectedRoom if its the current room
+    if (roomName === state.selectedRoom) {
+      state.selectedRoom = ""
+    }
+
+    if (state.roomListDisabled.indexOf(roomName) === -1){
+      state.roomListDisabled.push(roomName)
+    }
+
+    localStorage.setItem("roomListDisabled", JSON.stringify(roomListDisabled));
+  },
+  removeDisableRoom (state, roomName) {
+    let indice = state.roomListDisabled.indexOf(roomName);
+    if (indice !== undefined){
+      state.roomListDisabled.splice(indice, 1);
+      localStorage.setItem("roomListDisabled", JSON.stringify(state.roomListDisabled));
+    }
   }
 }
 
