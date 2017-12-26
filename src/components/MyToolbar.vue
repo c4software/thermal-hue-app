ea150015a67876ab3b61ff8f7b92ef9c2f22b70f
Valentin Brosseau <c4software@gmail.com>
2017-12-26 15:55:00 +0100

    Add components to navigate

---
+++ src/components/MyToolbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" clipped fixed app>
+      <myContentDrawer />
+    </v-navigation-drawer>
+
+    <v-toolbar class="indigo" clipped-left app>
+      <v-toolbar-side-icon dark @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title @click="goHome" class="white--text">Thermal Hue App</v-toolbar-title>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+import myContentDrawer from '@/components/Drawer'
+
+export default {
+  name: 'myToolbar',
+  components: {myContentDrawer},
+  data: function () {
+    return {
+      drawer: false
+    }
+  },
+  methods: {
+    goHome () {
+      window.location.hash = '/'
+    }
+  }
+}
+</script>
